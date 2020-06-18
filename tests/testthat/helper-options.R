cef008471f24827abfc2f1f392f06aae6f7146dc
Matthias <matthias.gomolka@posteo.de>
2020-06-18 22:10:11 +0200

    now assuming the environment variable SIMFIN_API_KEY
    This makes testing consistent locally and with GitHub Actions

--- tests/testthat/helper-options.R
+++ tests/testthat/helper-options.R
@@ -1,26 +1,2 @@
-sfa_api_key <- Sys.getenv("SIMFIN_API_KEY")
-# sfa_api_key <- Sys.getenv("FAKE_SECRET")
-
-
-if (sfa_api_key == "") {
-  tryCatch(
-    options(
-      sfa_api_key = secret::get_secret(
-        "sfa_api_key",
-        vault = "../../inst/vault"
-      )
-    ),
-    error = function(error) {
-      options(
-        sfa_api_key = secret::get_secret(
-          "sfa_api_key",
-          vault = "../../simfinapi/vault"
-        )
-      )
-    }
-  )
-} else {
-  options(sfa_api_key = sfa_api_key)
-}
-
-options(sfa_cache_dir = tempdir())
+options(sfa_api_key = Sys.getenv("SIMFIN_API_KEY"))
+options(sfa_cache_dir = file.path(tempdir()))
