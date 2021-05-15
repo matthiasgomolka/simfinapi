338bf40f580ab426cd89c59c9e435b9479424643
Matthias <matthias.gomolka@posteo.de>
2021-05-15 16:40:36 +0200

    recurring params only documented in param_doc (and referenced from there)

--- R/sfa_get_ref.R
+++ R/sfa_get_ref.R
@@ -1,18 +1,22 @@
 #' Download reference data
+#'
+#' @description Download reference data on industries and markets.
+#'
+#' @inheritParams param_doc
+#'
 #' @param ref_data [character] Either "industries" or "markets".
-# @param cache_dir [character] Your cache directory. It's recommended to set
-#   the cache directory globally using [sfa_set_cache_dir].
-#' @param api_key `[character(1)]` Your 'SimFin' API key. For simplicity use
-#'   `options(sfa_api_key = "yourapikey")`.
+#'
+#' @return [data.table] containing reference data on "industries" or "markets",
+#'   depending on `ref_data`.
+#'
 #' @importFrom utils download.file unzip
 #' @importFrom data.table fread
+#'
 #' @export
-sfa_get_ref <- function(
-  ref_data,
-  api_key = getOption("sfa_api_key")
-  # cache_dir = getOption("sfa_cache_dir") # NOT USED YET
-) {
-  check_inputs(ref_data = ref_data) #, cache_dir = cache_dir)
+#'
+sfa_get_ref <- function(ref_data, api_key = getOption("sfa_api_key")) {
+
+  check_inputs(ref_data = ref_data)
 
   temp_zip <- tempfile(fileext = ".zip")
   utils::download.file(
@@ -30,5 +34,7 @@ sfa_get_ref <- function(
     utils::unzip(temp_zip, list = TRUE)[, "Name"]
   )
 
-  data.table::fread(temp_csv, encoding = "UTF-8")
+  DT <- data.table::fread(temp_csv, sep = ";", encoding = "UTF-8")
+  set_clean_names(DT)
+  return(DT)
 }
